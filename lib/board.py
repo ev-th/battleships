3de208bac04eefb571c2dc8a1d4e61f3e8d126b7
Evan Thomas <evanthomas0292@gmail.com>
2023-08-08 22:28:03 +0100

    Write Board#init and #place without error handling

--- lib/board.py
+++ lib/board.py
@@ -1,12 +1,18 @@
 class Board:
-    def __init__(self):
-        # self.unplaced_ships
-        # self.ship_grid
-        # self.shot_grid
-        pass
+    def __init__(self, ships):
+        self.unplaced_ships = ships
+        self.ship_grid = [[None] * 10 for _ in range(10)]
+        self.shot_grid = [[False] * 10 for _ in range(10)]
 
-    def place(ship, position, orientation):
-        pass
+    def place(self, ship, row, column, orientation):
+        if orientation == 'vertical':
+            for i in range(ship.length):
+                self.ship_grid[row + i][column] = ship
+        if orientation == 'horizontal':
+            for i in range(ship.length):
+                self.ship_grid[row][column + i] = ship
+
+        self.unplaced_ships.remove(ship)
 
     def shoot(position):
         pass
---
+++ tests/test_board.py
@@ -0,0 +1,178 @@
+from lib.board import Board
+from unittest.mock import Mock
+
+def test_is_initialized_with_a_list_of_unplaced_ships():
+    ship1 = Mock()
+    ship2 = Mock()
+    ship3 = Mock()
+    ships = [ship1, ship2, ship3]
+
+    board = Board(ships)
+
+    assert board.unplaced_ships == ships
+
+def test_is_initialized_with_an_empty_ship_grid():
+    board = Board([])
+    expected_grid = [
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None]
+    ]
+    assert board.ship_grid == expected_grid
+
+def test_is_initialized_with_an_unhit_shot_grid():
+    board = Board([])
+    expected_grid = [
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False]
+    ]
+    assert board.shot_grid == expected_grid
+
+def test_places_a_length_2_ship_on_the_top_left_corner_vertically():
+    ship = Mock()
+    ship.length = 2
+    board = Board([ship])
+    board.place(ship, 0, 0, 'vertical')
+    expected_grid = [
+        [ship, None, None, None, None, None, None, None, None, None],
+        [ship, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None]
+    ]
+    assert board.ship_grid == expected_grid
+
+def test_places_a_length_3_ship_on_the_bottom_left_corner_horizontally():
+    ship = Mock()
+    ship.length = 3
+    board = Board([ship])
+    board.place(ship, 9, 0, 'horizontal')
+    expected_grid = [
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [ship, ship, ship, None, None, None, None, None, None, None]
+    ]
+    assert board.ship_grid == expected_grid
+
+def test_places_a_length_4_ship_on_the_top_right_corner_horizontally():
+    ship = Mock()
+    ship.length = 4
+    board = Board([ship])
+    board.place(ship, 0, 6, 'horizontal')
+    expected_grid = [
+        [None, None, None, None, None, None, ship, ship, ship, ship],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None]
+    ]
+    assert board.ship_grid == expected_grid
+
+def test_places_a_length_5_ship_on_the_bottom_right_corner_vertically():
+    ship = Mock()
+    ship.length = 5
+    board = Board([ship])
+    board.place(ship, 5, 9, 'vertical')
+    expected_grid = [
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, ship],
+        [None, None, None, None, None, None, None, None, None, ship],
+        [None, None, None, None, None, None, None, None, None, ship],
+        [None, None, None, None, None, None, None, None, None, ship],
+        [None, None, None, None, None, None, None, None, None, ship]
+    ]
+    assert board.ship_grid == expected_grid
+
+def test_places_several_ships_on_the_board():
+    ship1 = Mock()
+    ship1.length = 2
+    ship2 = Mock()
+    ship2.length = 3
+    ship3 = Mock()
+    ship3.length = 3
+    ship4 = Mock()
+    ship4.length = 4
+    ship5 = Mock()
+    ship5.length = 5
+
+    ships = [ship1, ship2, ship3, ship4, ship5]
+    board = Board(ships)
+
+    board.place(ship1, 0, 1, 'vertical')
+    board.place(ship2, 2, 3, 'horizontal')
+    board.place(ship3, 5, 6, 'horizontal')
+    board.place(ship4, 6, 5, 'vertical')
+    board.place(ship5, 1, 2, 'vertical')
+
+    expected_grid = [
+        [None, ship1, None, None, None, None, None, None, None, None],
+        [None, ship1, ship5, None, None, None, None, None, None, None],
+        [None, None, ship5, ship2, ship2, ship2, None, None, None, None],
+        [None, None, ship5, None, None, None, None, None, None, None],
+        [None, None, ship5, None, None, None, None, None, None, None],
+        [None, None, ship5, None, None, None, ship3, ship3, ship3, None],
+        [None, None, None, None, None, ship4, None, None, None, None],
+        [None, None, None, None, None, ship4, None, None, None, None],
+        [None, None, None, None, None, ship4, None, None, None, None],
+        [None, None, None, None, None, ship4, None, None, None, None]
+    ]
+    assert board.ship_grid == expected_grid
+
+def test_placing_ship_removes_it_from_unplaced_ships():
+    ship1 = Mock()
+    ship1.length = 2
+    ship2 = Mock()
+    ship2.length = 3
+
+    board = Board([ship1, ship2])
+    board.place(ship1, 0, 0, 'vertical')
+    assert board.unplaced_ships == [ship2]
+
+def test_placing_multiple_ships_removes_them_from_unplaced_ships():
+    ship1 = Mock()
+    ship1.length = 2
+    ship2 = Mock()
+    ship2.length = 3
+    ship3 = Mock()
+    ship3.length = 5
+
+    board = Board([ship1, ship2, ship3])
+    board.place(ship2, 0, 0, 'vertical')
+    board.place(ship3, 0, 1, 'vertical')
+    assert board.unplaced_ships == [ship1]
