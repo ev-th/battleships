f8521f7b4005616254b3e6f1caa2840e3f457e86
Evan Thomas <evanthomas0292@gmail.com>
2023-08-14 12:52:01 +0100

    Write Game tests and implementation

--- lib/game.py
+++ lib/game.py
@@ -6,26 +6,37 @@ class Game:
             {"name": player2, "board": board_class()}
         ]
 
-    def get_unplaced_ships():
-        pass
+    def get_unplaced_ships(self):
+        board = self.get_current_player_board()
+        return board.unplaced_ships
 
-    def place_ship(self, row, column, length):
-        pass
+    def place_ship(self, ship, row, column, orientation):
+        board = self.get_current_player_board()
+        board.place(ship, row, column, orientation)
 
     def shoot(self, row, column):
-        pass
+        board = self.get_current_opponent_board()
+        return board.shoot(row, column)
 
     def get_current_player_name(self):
-        pass
+        current_player_index = self.turn_count % 2
+        return self.players[current_player_index]["name"]
 
     def get_current_player_board(self):
-        pass
+        current_player_index = self.turn_count % 2
+        return self.players[current_player_index]["board"]
 
     def get_current_opponent_board(self):
-        pass
+        opponent_player_index = (self.turn_count + 1) % 2
+        return self.players[opponent_player_index]["board"]
 
     def get_winner(self):
-        pass
+        if self.players[0]["board"].is_defeated():
+            return self.players[1]["name"]
+        elif self.players[1]["board"].is_defeated():
+            return self.players[0]["name"]
+        else:
+            return None
 
-    def next_turn():
-        pass
+    def next_turn(self):
+        self.turn_count += 1
--- tests/test_game.py
+++ tests/test_game.py
@@ -15,58 +15,251 @@ def test_game_is_initilized_with_two_boards():
         ]
 
 def test_get_unplaced_ships_first_gets_player_1_unplaced_ships():
-    pass
+    ships = Mock()
+    board_object1 = Mock()
+    board_object1.unplaced_ships = ships
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    result = game.get_unplaced_ships()
+
+    assert result == ships
 
 def test_get_unplaced_ships_gets_player_2_unplaced_ships_after_next_turn_is_called():
-    pass
+    ships = Mock()
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_object2.unplaced_ships = ships
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    game.next_turn()
+    result = game.get_unplaced_ships()
+
+    assert result == ships
 
 def test_place_ship_first_places_ship_on_player_1_board():
-    pass
+    ship = Mock()
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    game.place_ship(ship, 6, 2, 'vertical')
+
+    board_object1.place.assert_called_once_with(ship, 6, 2, 'vertical')
 
 def test_place_ship_places_ship_on_player_2_board_after_next_turn_is_called():
-    pass
+    ship = Mock()
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    game.next_turn()
+    game.place_ship(ship, 6, 2, 'vertical')
+
+    board_object2.place.assert_called_once_with(ship, 6, 2, 'vertical')
 
 def test_shoot_first_shoots_player_2_board():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    game.shoot(7, 9)
+
+    board_object2.shoot.assert_called_once_with(7, 9)
 
 def test_shoot_shoots_player_1_board_after_next_turn_is_called():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    game.shoot(7, 9)
+    game.next_turn()
+    game.shoot(3, 4)
+
+    board_object1.shoot.assert_called_once_with(3, 4)
 
 def test_shoots_player_2_board_after_next_turn_is_called_twice():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    game.shoot(7, 9)
+    game.next_turn()
+    game.shoot(3, 4)
+    game.next_turn()
+    game.shoot(2, 1)
+
+    board_object1.shoot.assert_called_with(3, 4)
 
 def test_shoot_returns_the_return_value_of_shooting_a_board():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_object2.shoot.return_value = "miss"
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    result = game.shoot(1, 2)
+
+    assert result == "miss"
 
 def test_get_current_player_name_first_gets_player_1_name():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    result = game.get_current_player_name()
+
+    assert result == "player1"
 
 def test_get_current_player_name_gets_player_2_name_after_next_turn_is_called():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    game.next_turn()
+    result = game.get_current_player_name()
+
+    assert result == "player2"
 
 def test_get_current_player_name_first_gets_player_1_name_after_next_turn_is_called_twice():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    game.next_turn()
+    game.next_turn()
+    result = game.get_current_player_name()
+
+    assert result == "player1"
 
 def test_get_current_player_board_first_gets_player_1_board():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    result = game.get_current_player_board()
+    
+    assert result == board_object1
 
 def test_get_current_player_board_gets_player_2_board_after_next_turn_is_called():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    game.next_turn()
+    result = game.get_current_player_board()
+
+    assert result == board_object2
 
 def test_get_current_player_board_first_gets_player_1_board_after_next_turn_is_called_twice():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    game.next_turn()
+    game.next_turn()
+    result = game.get_current_player_board()
+
+    assert result == board_object1
 
 def test_get_current_opponent_board_first_gets_player_2_board():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    result = game.get_current_opponent_board()
+
+    assert result == board_object2
 
 def test_get_current_opponent_board_gets_player_1_board_after_next_turn_is_called():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    game.next_turn()
+    result = game.get_current_opponent_board()
+
+    assert result == board_object1
 
 def test_get_current_opponent_board_first_gets_player_2_board_after_next_turn_is_called_twice():
-    pass
+    board_object1 = Mock()
+    board_object2 = Mock()
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
 
-def test_get_winner_returns_player_name_if_opponents_board_is_defeated():
-    pass
+    game.next_turn()
+    game.next_turn()
+    result = game.get_current_opponent_board()
+
+    assert result == board_object2
+
+def test_get_winner_returns_player_1_if_player_2_board_is_defeated():
+    board_object1 = Mock()
+    board_object1.is_defeated.return_value = False
+    board_object2 = Mock()
+    board_object2.is_defeated.return_value = True
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    result = game.get_winner()
+
+    assert result == "player1"
+
+def test_get_winner_returns_player_2_if_player_1_board_is_defeated():
+    board_object1 = Mock()
+    board_object1.is_defeated.return_value = True
+    board_object2 = Mock()
+    board_object2.is_defeated.return_value = False
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    result = game.get_winner()
+
+    assert result == "player2"
 
 def test_get_winner_returns_None_name_if_neither_board_is_defeated():
-    pass
+    board_object1 = Mock()
+    board_object1.is_defeated.return_value = False
+    board_object2 = Mock()
+    board_object2.is_defeated.return_value = False
+    board_class = Mock()
+    board_class.side_effect = [board_object1, board_object2]
+    game = Game("player1", "player2", board_class)
+
+    result = game.get_winner()
+
+    assert result is None
