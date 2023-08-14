d35a49966b5313244703d4188c533e7ed61c72b5
Evan Thomas <evanthomas0292@gmail.com>
2023-08-14 16:11:16 +0100

    Write BoardFormatter

--- lib/board_formatter.py
+++ lib/board_formatter.py
@@ -1,3 +1,55 @@
 class BoardFormatter:
-    def display():
-        pass
+    @classmethod
+    def display_ships(cls, board):
+
+        top_line = "  | A | B | C | D | E | F | G | H | I | J |"
+        divider = "--|---+---+---+---+---+---+---+---+---+---|"
+        bottom_line = "------------------------------------------+"
+
+        rows = [top_line]
+        for i, row in enumerate(board.ship_grid):
+            row_number = str(i + 1)
+            if (i + 1) < 10:
+                row_number += " "
+
+            positions = []
+            for position in row:
+                if position is None:
+                    positions.append("   ")
+                else:
+                    positions.append(" S ")
+            
+            row_str = row_number + "|" + "|".join(positions) + "|"
+            rows.append(divider)
+            rows.append(row_str)
+        rows.append(bottom_line)
+        return "\n".join(rows)
+
+
+    @classmethod
+    def display_shots(cls, board):
+        
+        top_line = "  | A | B | C | D | E | F | G | H | I | J |"
+        divider = "--|---+---+---+---+---+---+---+---+---+---|"
+        bottom_line = "------------------------------------------+"
+
+        rows = [top_line]
+        for i in range(board.grid_size):
+            row_number = str(i + 1)
+            if (i + 1) < 10:
+                row_number += " "
+
+            positions = []
+            for j in range(board.grid_size):
+                if board.shot_grid[i][j]:
+                    if board.ship_grid[i][j]:
+                        positions.append(" H ")
+                    else:
+                        positions.append(" M ")
+                else:
+                    positions.append("   ")
+            row_str = row_number + "|" + "|".join(positions) + "|"
+            rows.append(divider)
+            rows.append(row_str)
+        rows.append(bottom_line)
+        return "\n".join(rows)
---
+++ tests/test_board_formatter.py
@@ -0,0 +1,198 @@
+from lib.board_formatter import BoardFormatter
+from unittest.mock import Mock
+
+def test_display_ships_displays_empty_ship_grid():
+    ship_grid = [
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+    ]
+    board = Mock()
+    board.ship_grid = ship_grid
+
+    result = BoardFormatter.display_ships(board)
+
+    expected_result = '  | A | B | C | D | E | F | G | H | I | J |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '1 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '2 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '3 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '4 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '5 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '6 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '7 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '8 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '9 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '10|   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '------------------------------------------+'
+    
+    assert result == expected_result
+
+def test_display_ships_displays_multiple_ships():
+    ship1 = Mock()
+    ship2 = Mock()
+    ship3 = Mock()
+    ship_grid = [
+        [None, ship1, ship1, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, ship2, None, None, None, None, None],
+        [None, None, None, None, ship2, None, None, None, None, None],
+        [None, None, None, None, ship2, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, ship3, ship3, ship3, ship3, None],
+    ]
+    board = Mock()
+    board.ship_grid = ship_grid
+
+    result = BoardFormatter.display_ships(board)
+
+    expected_result = '  | A | B | C | D | E | F | G | H | I | J |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '1 |   | S | S |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '2 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '3 |   |   |   |   | S |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '4 |   |   |   |   | S |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '5 |   |   |   |   | S |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '6 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '7 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '8 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '9 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '10|   |   |   |   |   | S | S | S | S |   |\n'
+    expected_result += '------------------------------------------+'
+    
+    assert result == expected_result
+
+def test_display_shots_displays_empty_shot_grid():
+    shot_grid = [
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False]
+    ]
+
+    board = Mock()
+    board.grid_size = 10
+    board.shot_grid = shot_grid
+
+    result = BoardFormatter.display_shots(board)
+
+    expected_result = '  | A | B | C | D | E | F | G | H | I | J |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '1 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '2 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '3 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '4 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '5 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '6 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '7 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '8 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '9 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '10|   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '------------------------------------------+'
+
+    assert result == expected_result
+
+def test_display_shots_displays_shots():
+    shot_grid = [
+        [False, True, True, True, True, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, False, False],
+        [False, False, False, False, False, False, False, False, True, True]
+    ]
+
+    ship1 = Mock()
+    ship2 = Mock()
+    ship3 = Mock()
+    ship_grid = [
+        [None, ship1, ship1, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, ship2, None, None, None, None, None],
+        [None, None, None, None, ship2, None, None, None, None, None],
+        [None, None, None, None, ship2, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, ship3, ship3, ship3, ship3, None],
+    ]
+
+    board = Mock()
+    board.ship_grid = ship_grid
+    board.shot_grid = shot_grid
+    board.grid_size = 10
+
+    result = BoardFormatter.display_shots(board)
+
+    expected_result = '  | A | B | C | D | E | F | G | H | I | J |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '1 |   | H | H | M | M |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '2 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '3 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '4 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '5 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '6 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '7 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '8 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '9 |   |   |   |   |   |   |   |   |   |   |\n'
+    expected_result += '--|---+---+---+---+---+---+---+---+---+---|\n'
+    expected_result += '10|   |   |   |   |   |   |   |   | H | M |\n'
+    expected_result += '------------------------------------------+'
+
+    assert expected_result == result
